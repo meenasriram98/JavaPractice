80c4ee1cd1d7f36d1c8688ab3f3b9993426fe829
meenasriram98 <48787413+meenasriram98@users.noreply.github.com>
2019-07-04 12:25:35 +0530

    Add files via upload
    added maven project

---
+++ src/main/java/assignment2/services/GroupService.java
@@ -0,0 +1,156 @@
+package assignment2.services;
+
+import java.util.List;
+import java.util.Scanner;
+
+import assignment2.classes.Group;
+import assignment2.classes.User;
+import assignment2.repositories.GroupRepository;
+
+public class GroupService {
+	Scanner sc=new Scanner(System.in);
+	
+	UserService userService=new UserService();
+	GroupRepository grouprepository=GroupRepository.getInstance();
+	
+	public Group createGroup()
+	{
+		System.out.println("enter group name");
+		String name=sc.nextLine();
+		
+		System.out.println("enter group id");
+		int id=sc.nextInt();
+		
+		Group group=new Group(name,id);
+		return group;
+	}
+	
+	public boolean addGroup()
+	{
+		Group group=createGroup();
+		grouprepository.addGroup(group);
+		System.out.println("group created");
+		return true;
+	}
+	
+	public void removeGroup()
+	{
+		System.out.println("Enter group name");
+		String name=sc.nextLine();
+		
+		if(!groupPresent(name))
+		{
+			System.out.println("group not present");
+		}
+		else
+		{
+		removeGroupFromGroup(name);
+		userService.removeGroupFromUser(name);
+		grouprepository.removeGroup(name);
+		System.out.println("group removed");
+		}
+		
+	}
+	
+	public void removeGroupFromGroup(String name) {
+		
+			List<Group> groups=grouprepository.getGroupList();
+			
+			groups.forEach(group->{group.getJoinedGroups().removeIf(x->x.equals(name));
+			grouprepository.updateRepository(group);});
+
+		
+	}
+
+	public void addUserToGroup()
+	{
+		System.out.println("Enter user name");
+		String userName=sc.nextLine();
+		
+		System.out.println("Enter group name");
+		String groupName=sc.nextLine();
+		
+		if(userService.userPresent(userName)&&groupPresent(groupName))
+		{
+			saveUserToGroup(userName,groupName);
+			System.out.println("user added to group");
+		}
+		else
+		{
+			System.out.println("invalid info entered");
+		}
+		
+	}
+	
+	public void saveUserToGroup(String userName,String groupName)
+	{
+		Group group=grouprepository.getGroupObject(groupName);
+		group.getUserMembers().add(userService.getUserObject(userName));
+		grouprepository.updateRepository(group);
+	}
+	
+	public void removeUserFromGroup()
+	{
+		System.out.println("Enter user name");
+		String userName=sc.nextLine();
+		
+		System.out.println("Enter group name");
+		String groupName=sc.nextLine();
+		
+		if(userService.userPresent(userName)&&groupPresent(groupName))
+		{
+			removeUser(userName,groupName);
+		}
+		else
+		{
+			System.out.println("invalid info entered");
+		}
+	}
+	
+	public void removeUser(String userName, String groupName) {
+		Group group=grouprepository.getGroupObject(groupName);
+		group.getUserMembers().remove(userService.getUserObject(userName));
+		grouprepository.updateRepository(group);
+		
+	}
+
+	public void addGroupToGroup()
+	{
+		System.out.println("Enter group name");
+		String group=sc.nextLine();
+		
+		System.out.println("Enter group name to be added");
+		String memberGroup=sc.nextLine();
+		
+		if(groupPresent(group)&&groupPresent(memberGroup))
+		{
+			saveGroup(group,memberGroup);
+			System.out.println("group added");
+		}
+		else
+		{
+			System.out.println("invalid info entered");
+		}
+	}
+	
+	public void saveGroup(String group, String memberGroup) {
+		
+		Group g=grouprepository.getGroupObject(group);
+		g.getJoinedGroups().add(grouprepository.getGroupObject(memberGroup));
+		grouprepository.updateRepository(g);
+		
+	}
+
+	public boolean groupPresent(String name)
+	{
+		return grouprepository.getGroup(name);
+	}
+
+	public void removeUserFromAllGroups(String name)
+	{
+		List<Group> groups=grouprepository.getGroupList();
+		
+		groups.forEach(group->{group.getUserMembers().removeIf(x->x.equals(name));
+		grouprepository.updateRepository(group);});
+	}
+}
---
+++ src/test/java/assignment2/tests/GroupServiceTest.java
@@ -0,0 +1,76 @@
+package assignment2.tests;
+
+
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import assignment2.classes.Group;
+import assignment2.repositories.GroupRepository;
+import assignment2.repositories.UserRepository;
+
+public class GroupServiceTest {
+	//GroupService groupService=new GroupService();
+	GroupRepository grouprepository=GroupRepository.getInstance();
+	UserRepository usersrepository=UserRepository.getInstance();
+	Group g=new Group("g",12);
+	Group gr=new Group("gsd",14);
+	
+	@Test
+	public void testCreateGroup() {
+		System.out.println(gr.getName());
+		assertEquals("gsd",gr.getName());
+	}
+
+
+	@Test
+	public void testAddGroup() {
+		grouprepository.addGroup(g);
+		grouprepository.addGroup(gr);
+		assertEquals(2,grouprepository.getSize());
+	}
+	
+	@Test
+	public void testRemoveGroup() {
+		grouprepository.removeGroup(g.getName());
+		assertEquals(0,grouprepository.getSize());
+	}
+
+	
+	public void testRemoveGroup1() {
+		grouprepository.removeGroup("sdkfhu");
+		assertEquals(2,grouprepository.getSize());
+	}
+
+	@Test
+	public void testAddGroupToGroup() {
+		Group gr=new Group("g4",19);
+		Group g=new Group("g5",3);
+		
+		grouprepository.addGroup(gr);
+		grouprepository.addGroup(g);
+		
+		g.getJoinedGroups().add(grouprepository.getGroupObject(gr.getName()));
+		grouprepository.updateRepository(g);
+		Group group=grouprepository.getGroupObject(g.getName());
+		assertEquals(true,group.getJoinedGroups().contains(gr));
+	}
+	
+
+	@Test
+	public void testGroupPresent() {
+		Group gr=new Group("g2",13);
+		grouprepository.addGroup(gr);
+		assertTrue(grouprepository.getGroup("g2"));
+	}
+	
+	@Test
+	public void testGroupPresent2() {
+		assertFalse(grouprepository.getGroup("meena"));
+	}
+
+
+
+
+}
